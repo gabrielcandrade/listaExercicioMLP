cbf65063b828a9974cf4c60cbfdc416bc1ff7f90
Gabriel Andrade <gabrielcandrade01@gmail.com>
2019-03-09 00:24:31 -0300

    Fixing

--- src/br/unipe/java/questao01/Main.java
+++ src/br/unipe/java/questao01/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		float salario, novoSalario;
-		int percentual;
+		float salario, novoSalario, percentual, aumento;
 		
 		Scanner leitor = new Scanner(System.in);
 		System.out.println("Digite o seu salário: ");
@@ -14,9 +13,12 @@ public class Main {
 		System.out.println("Digite o seu percentual de aumento: ");
 		percentual = leitor.nextInt();
 		
-		novoSalario = salario + (salario * percentual/100);
+		aumento = (float) (salario * percentual/100.0);
 		
-		System.out.println("O novo salario será de " + novoSalario + "reais");
+		novoSalario = (float) (salario + aumento);
+		
+		System.out.println("O aumento foi de: " + aumento);		
+		System.out.println("O novo salario será de " + novoSalario + " reais");
 		leitor.close();
 	}
 
